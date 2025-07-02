ee51fabead57948a8844102e0bdacc2527461313
codermq06 <qazifromch@gmail.com>
2025-07-03 00:35:09 +0200

    Email Task

--- lesson1/practice.py
+++ lesson1/practice.py
@@ -1,3 +1,7 @@
-my_list = ["Charles", "Susie", "Sid"]
-my_list.remove("Susie")
-print(my_list)
+
+def giveaway(ilist):
+    print(str(ilist[0]) + " is the winner")
+    print(str(ilist[-1]) + " is the winner")
+    print(str(ilist[9]) + " is the winner")
+    print(str(ilist[19]) + " is the winner")
+giveaway([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
